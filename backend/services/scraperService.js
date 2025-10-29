d739ae3d54bb77f4690f43ab63e12f6186878d52
lambertR <rune.lambert@student.vives.be>
2025-10-29 16:03:09 +0100

    mongoose fix

--- backend/services/scraperService.js
+++ backend/services/scraperService.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
+const MONGO_URI = process.env.MONGO_URI
 /**
  * Scrapes all brands, models, categories, and parts from the site.
  */
